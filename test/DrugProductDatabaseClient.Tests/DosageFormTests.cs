ed02f2cf03345b903233f8e23d1e1f2e8f68c374
Kori Francis <kfrancis@clinicalsupportsystems.com>
2021-07-20 14:49:31 -0400

    individual test classes

--- test/DrugProductDatabaseClient.Tests/DosageFormTests.cs
+++ test/DrugProductDatabaseClient.Tests/DosageFormTests.cs
@@ -3,23 +3,16 @@ using System;
 using System.Threading.Tasks;
 using Xunit;
 using Shouldly;
-using Xunit.Abstractions;
 
 namespace DrugProductDatabaseClient.Tests
 {
-    public class ActiveIngredientTests : TestBase
+    public class DosageFormTests
     {
-        public ActiveIngredientTests(ITestOutputHelper output) : base(output)
-        {
-        }
-
         [Fact]
         public async Task Test1()
         {
             var drugProduct = await DrugProductRequest.GetDrugProduct(din: "02313782");
             drugProduct.ShouldNotBeNull();
-
-            Output.WriteLine(await GetJsonAsync(drugProduct));
         }
     }
 }
---
+++ test/DrugProductDatabaseClient.Tests/TestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace DrugProductDatabaseClient.Tests
+{
+    public class TestBase
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TestBase(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public ITestOutputHelper Output => _output;
+
+        public async Task<string> GetJsonAsync(object obj, JsonSerializerOptions jsonSerializerOptions = null, CancellationToken ct = default)
+        {
+            ct.ThrowIfCancellationRequested();
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, obj, obj.GetType(), jsonSerializerOptions ?? new JsonSerializerOptions() { WriteIndented = true }, ct).ConfigureAwait(false);
+            stream.Position = 0;
+            using var reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync().ConfigureAwait(false);
+        }
+    }
+}
