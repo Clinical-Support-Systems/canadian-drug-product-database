98de6977a63ae6a0a98c1233a1ca56986dcd551e
Kori Francis <kfrancis@clinicalsupportsystems.com>
2021-07-20 14:33:41 -0400

    Add some xunit tests

---
+++ test/DrugProductDatabaseClient.Tests/ActiveIngredientTests.cs
@@ -0,0 +1,18 @@
+using DrugProductDatabase;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace DrugProductDatabaseClient.Tests
+{
+    public class ActiveIngredientTests
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var drugProduct = await DrugProductRequest.GetDrugProduct(din: "02313782");
+            drugProduct.ShouldNotBeNull();
+        }
+    }
+}
